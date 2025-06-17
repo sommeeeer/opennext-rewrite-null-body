f2ec67ed84678ad4cda1ec6b60256008de4ceb7e
Magnus Dahl Eide <magnus@dahleide.com>
2025-06-17 14:57:22 +0200

    add src2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   async rewrites() {
     return {
-      beforeFiles: [{ source: '/src', destination: `http://localhost:4000` }],
+      beforeFiles: [
+        { source: '/src', destination: `http://localhost:4000` },
+        { source: '/src2', destination: 'http://localhost:8787/dest' },
+      ],
     };
   },
 };
